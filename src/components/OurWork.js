7606bf519d1565b04700bb4107e34e949802beb9
andurebel <andu.trica@gmail.com>
2022-02-28 02:29:57 +0200

    basic search done

--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -1,27 +1,42 @@
 import json from "../data.json";
 import Card from "./Card";
-import Form from "./Form";
+import { useState } from "react";
 
 export const OurWork = () => {
   const { data } = json;
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <>
-      <main className="w-full bg-orange-100">
-        <div className="container mx-auto py-10 ">
+      <main className="w-full bg-orange-100 flex ">
+        <div className="container mx-auto py-10 flex flex-col  justify-center">
           <h1 className="text-2xl font-bold text-center py-10">
             Check out our products
           </h1>
-          <Form data={data} />
+          <div className="text-center">
+            <input
+              type="text"
+              value={searchTerm}
+              placeholder="Search..."
+              className="p-2 w-3/4 shadow rounded  "
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+          </div>
           <ul className="grid md:grid-cols-3 gap-8 p-10">
-            {data.map((article) => (
-              <Card
-                key={article.sku}
-                name={article.name}
-                description={article.description}
-                image={article.main_image}
-              />
-            ))}
+            {data
+              .filter((article) => {
+                return article.name
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase());
+              })
+              .map((article) => (
+                <Card
+                  key={article.sku}
+                  name={article.name}
+                  description={article.description}
+                  image={article.main_image}
+                />
+              ))}
           </ul>
         </div>
       </main>
