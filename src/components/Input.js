6b74d1dfde422529d07c3454304988600062c823
andurebel <andu.trica@gmail.com>
2022-03-03 00:47:20 +0200

    input button added conditionnally

--- src/components/Input.js
+++ src/components/Input.js
@@ -18,12 +18,14 @@ export const Input = ({ filteredData }) => {
         } py-2.5 px-2 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0  peer`}
         placeholder="Enter Search here"
       />
-      <button
-        className="text-white bg-purple-700 hover:bg-purple-600 mt-8  px-4 py-2 text-xl rounded-xl hover:shadow "
-        onClick={() => setSearchTerm("")}
-      >
-        Clear Search
-      </button>
+      {searchTerm && (
+        <button
+          className="text-white bg-purple-700 hover:bg-purple-600 mt-8  px-4 py-2 text-xl rounded-xl hover:shadow "
+          onClick={() => setSearchTerm("")}
+        >
+          Clear Search
+        </button>
+      )}
     </>
   );
 };
